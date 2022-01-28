a12bbf4318753c2d5e09c406a6a59ec619c786ed
errea <erreakezy@gmail.com>
2022-01-28 14:09:11 +0100

    Add suooport directory to not load by default

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,6 +46,10 @@ Shoulda::Matchers.configure do |config|
 end
 
 
+# [...]
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -54,7 +58,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+ # [...]
+ config.include RequestSpecHelper, type: :request
+ # [...]
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
